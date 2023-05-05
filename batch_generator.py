f7a3ddc6badbbd21e980a4154de4b002ad18cd87
Srinitish Srinivasan <srinitishsri@gmail.com>
2023-05-05 18:54:35 +0530

    Batch generator

---
+++ batch_generator.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+from image_generator import generate_image
+
+
+def batch_generator(paths, batch_size):
+    while True:
+        image = generate_image(paths)
+
+        img_batch = list()
+        mask_batch = list()
+
+        for sample, mask in image:
+            img_batch.append(sample)
+            mask_batch.append(mask)
+
+            if (len(img_batch) == batch_size):
+                yield np.stack(img_batch, axis=0), np.stack(mask_batch, axis=0)
+                img_batch = []
+                mask_batch = []
+
+        if (len(img_batch != 0)):
+            yield np.stack(img_batch, axis=0), np.stack(mask_batch, axis=0)
+            img_batch = []
+            mask_batch = []
