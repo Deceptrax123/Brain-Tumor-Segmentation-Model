4e2081393ce4fef4515dbe6c396f7dc3535f12c5
Srinitish Srinivasan <srinitishsri@gmail.com>
2023-05-07 01:16:57 +0530

    fix image generator

--- image_generator.py
+++ image_generator.py
@@ -8,13 +8,12 @@ def get_id(path):
     slice = path.split(".")[1]
     id = slice[len(slice)-1:len(slice)-4:-1]
 
-    return id
+    return id[-1::-1]
 
 
 def generate_image(paths, size=(128, 128, 128)):
     for path in paths:
         example_id = get_id(path)
-
         sample = np.load(path)
         mask = np.load("./Data/Train/masks_reformatted/mask_" +
                        example_id+".npy")
