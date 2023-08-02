9a50710bda7bac6091dc9f2b6643eddf4423453c
Srinitish Srinivasan <srinitishsri@gmail.com>
2023-08-02 21:28:44 +0530

    changes

--- train.py
+++ train.py
@@ -32,19 +32,21 @@ def train(images, masks):
 @tf.function
 def test(images, masks):
     predictions = model(images, training=False)
+    test_loss = Complete_Dice_loss().call(masks, predictions)
 
     test_complete.update_state(masks, predictions)
     test_edema.update_state(masks, predictions)
     test_enhancing.update_state(masks, predictions)
     test_necrotic.update_state(masks, predictions)
 
+    return test_loss
+
 
 def training_loop(traingen, testgen, callbacks, train_steps, test_steps):
     num_epochs = 50
-    train_loss = 0
 
-    losses = []
     train_losses = []
+    test_losses = []
     # train metrics
     tcomplete = []
     tedema = []
@@ -56,19 +58,32 @@ def training_loop(traingen, testgen, callbacks, train_steps, test_steps):
     teedema = []
     tenecrotic = []
     teenhancing = []
-
     for epoch in range(num_epochs):
+        # get learning rate as per scheduler function
+        lr = scheduler(epoch+1)
+        optimizer.learning_rate = lr
+
+        traingen = batch_generator(train_sample_paths, batch_size)
+        testgen = batch_generator(test_sample_paths, batch_size)
+
+        print(
+            "--------------------------Start of Epoch %d---------------------------------" % (epoch+1))
+        losses = []
+        tlosses = []
         for step in range(train_steps):
+
             sample, masks = next(traingen)
 
             train_loss = train(sample, masks)
             losses.append(train_loss)
 
-            if (step % 10 == 0):
-                print("Loss so far per batch - %.4f" % (float(train_loss)))
+            if (step % 50 == 0):
+                print("Loss so far per 50 batches - %.4f" %
+                      (float(train_loss)))
 
         m.update_state(losses)
-        print("Training Loss after epoch % d - %.4f" % (float(m.result())))
+        print("Training Loss after epoch % d - %.4f" %
+              (epoch+1, float(m.result())))
 
         train_losses.append(m.result())
         m.reset_state()
@@ -76,15 +91,24 @@ def training_loop(traingen, testgen, callbacks, train_steps, test_steps):
         for step in range(test_steps):
             sample, masks = next(testgen)
 
-            test(sample, masks)
+            test_loss = test(sample, masks)
+            tlosses.append(test_loss)
+
+        test_mean.update_state(tlosses)
+        test_losses.append(test_mean.result())
+
+        print("Test Loss after epoch %d - %.4f" %
+              (epoch+1, float(test_mean.result())))
+
+        test_mean.reset_state()
 
         # metrics after training
         print("Training Metrics after epoch %d- Dice coef complete- %.4f,Enhancing- %0.4f, Necrotic- %0.4f, Edema- %0.4f" %
-              (epoch, train_complete.result()/train_steps, train_enhancing.result()/train_steps, train_necrotic.result()/train_steps, train_edema.result()/train_steps))
+              (epoch+1, train_complete.result()/train_steps, train_enhancing.result()/train_steps, train_necrotic.result()/train_steps, train_edema.result()/train_steps))
 
         # metrics after testing
         print("Test metrics after epoch %d- Dice Coef complete- %0.4f, Enhancing- %0.4f, Necrotic- %0.4f, Edema- %0.4f" %
-              (epoch, test_complete.result()/test_steps, test_enhancing.result()/test_steps, test_necrotic.result()/test_steps, test_edema.result()/test_steps))
+              (epoch+1, test_complete.result()/test_steps, test_enhancing.result()/test_steps, test_necrotic.result()/test_steps, test_edema.result()/test_steps))
 
         # Append to list to visualize later
         tcomplete.append(train_complete.result()/train_steps)
@@ -107,6 +131,8 @@ def training_loop(traingen, testgen, callbacks, train_steps, test_steps):
         test_edema.reset_state()
         test_necrotic.reset_state()
 
+        print("-------------------------End of Epoch %d --------------------------------" % (epoch+1))
+
     return train_losses, tcomplete, tedema, tenhancing, tecomplete, teedema, teenhancing, tenecrotic
 
 
@@ -116,9 +142,6 @@ masks_paths = sorted(glob("./Data/Train/masks_reformatted/*.npy"))
 
 train_sample_paths, test_sample_paths = train_test_split(
     samples_paths, test_size=0.25)
-
-traingen = batch_generator(train_sample_paths, batch_size)
-testgen = batch_generator(test_sample_paths, batch_size)
 
 train_steps = steps(len(train_sample_paths), batch_size)
 test_steps = steps(len(test_sample_paths), batch_size)
@@ -140,18 +163,4 @@ train_edema = Edema_Dice_Coef()
 test_edema = Edema_Dice_Coef()
 
 mean = tf.keras.metrics.Mean()
-
-log_dir = "/kaggle/working/logs/fit/" + \
-    datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = tf.keras.callbacks.TensorBoard(
-    log_dir=log_dir, histogram_freq=1)
-schedule = tf.keras.callbacks.LearningRateScheduler(scheduler)
-plateau_handler = tf.keras.callbacks.ReduceLROnPlateau(
-    monitor='val_loss', patience=3, factor=0.1)
-nanterminate = tf.keras.callbacks.TerminateOnNaN()
-earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss')
-checkpoints = tf.keras.callbacks.ModelCheckpoint(filepath="/kaggle/working/models/2pathcnn_bilstm",
-                                                 save_weights_only=False, save_freq='epoch', save_best_only=True, monitor='dice_coef', mode='max', verbose=1)
-
-callbacks = [tensorboard_callback, schedule,
-             plateau_handler, nanterminate, earlystop, checkpoints]
+test_mean = tf.keras.metrics.Mean()
