b88b9bbaf54ec15b87a2c01ec39f3f622af1426d
Srinitish Srinivasan <srinitishsri@gmail.com>
2023-08-04 20:19:25 +0530

    model

--- layers.py
+++ layers.py
@@ -13,17 +13,21 @@ class ConvBlockEnc(tf.keras.layers.Layer):
                            kernel_initializer='he_normal', use_bias=True, bias_initializer='he_normal')
         self.bn = BatchNormalization()
 
+        self.conv1 = Conv3D(filters=filters, kernel_size=kernel_size, padding=padding,
+                            kernel_initializer='he_normal', use_bias=True, bias_initializer='he_normal')
+        self.bn1 = BatchNormalization()
+
     def call(self, input, training=False):
         x = self.conv(input)
         x = self.bn(x)
         x = tf.nn.relu(x)
 
-        x = self.conv(x)
-        x = self.bn(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
         x = tf.nn.relu(x)
 
-        x = self.conv(x)
-        x = self.bn(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
         x = tf.nn.relu(x)
 
         return x
@@ -36,17 +40,21 @@ class ConvBlockDec(tf.keras.layers.Layer):
                                       kernel_initializer='he_normal', use_bias=True, bias_initializer='he_normal')
         self.bn = BatchNormalization()
 
+        self.deconv1 = Conv3DTranspose(filters=filters, kernel_size=kernel_size, padding=padding,
+                                       kernel_initializer='he_normal', use_bias=True, bias_initializer='he_normal')
+        self.bn1 = BatchNormalization()
+
     def call(self, input, training=False):
         x = self.deconv(input)
         x = self.bn(x)
         x = tf.nn.relu(x)
 
-        x = self.deconv(x)
-        x = self.bn(x)
+        x = self.deconv1(x)
+        x = self.bn1(x)
         x = tf.nn.relu(x)
 
-        x = self.deconv(x)
-        x = self.bn(x)
+        x = self.deconv1(x)
+        x = self.bn1(x)
         x = tf.nn.relu(x)
 
         return x
@@ -56,9 +64,24 @@ class Lstm(tf.keras.layers.Layer):
     def __init__(self, kernel_size, filters, padding='same'):
         super(Lstm, self).__init__()
         self.lstm = ConvLSTM2D(filters=filters, kernel_size=kernel_size, padding=padding, activation='tanh',
-                               recurrent_activation='hard_sigmoid', kernel_initializer='he_normal', use_bias=True, bias_initializer='he_normal')
+                               recurrent_activation='hard_sigmoid', kernel_initializer='he_normal', use_bias=True, bias_initializer='he_normal', return_sequences=True)
 
     def call(self, input, training=False):
         x = self.lstm(input)
+
+        return x
+
+
+class Final(tf.keras.layers.Layer):
+    def __init__(self, kernel_size, filters, padding='same'):
+        super(Final, self).__init__()
+        self.conv = Conv3D(filters=filters, kernel_size=kernel_size, kernel_initializer='he_normal',
+                           padding=padding, use_bias=True, bias_initializer='he_normal')
+        self.bn = BatchNormalization()
+
+    def call(self, input, training=False):
+        x = self.conv(input)
+        x = self.bn(x)
+        x = tf.nn.softmax(x)
 
         return x
--- models.py
+++ models.py
@@ -1,22 +1,25 @@
 from keras.layers import Conv3D, MaxPool3D, Flatten, Dense, Reshape, AveragePooling3D, Conv3DTranspose, UpSampling3D, Activation
-from keras.layers import Dropout, MaxPooling3D, Input, BatchNormalization, Add, Concatenate, ConvLSTM2D, Bidirectional, Attention
+from keras.layers import Input, Dropout, MaxPooling3D, BatchNormalization, Add, Concatenate, ConvLSTM2D, Bidirectional, Attention
 import tensorflow as tf
+from layers import ConvBlockEnc, ConvBlockDec, Lstm, Final
 from keras.models import Model
-from layers import ConvBlockEnc, ConvBlockDec, Lstm, Down
 
 
-class DualCNNLstm(tf.keras.models.Model):
+class DualPathCNNLstm(tf.keras.Model):
     def __init__(self):
-        super(DualCNNLstm, self).__init__()
+        super(DualPathCNNLstm, self).__init__()
 
+        self.input_layer = Conv3D(kernel_size=(3, 3, 3), use_bias=True, kernel_initializer='he_normal',
+                                  bias_initializer='he_normal', padding='same', filters=3)
+        self.convenc_start = ConvBlockEnc(kernel_size=(3, 3, 3), filters=4)
         self.convenc_1 = ConvBlockEnc(kernel_size=(3, 3, 3), filters=8)
         self.convenc_2 = ConvBlockEnc(kernel_size=(3, 3, 3), filters=16)
-        self.convenc_3 = ConvBlockEnc(kernel_size=(3, 3, 3), filter=32)
+        self.convenc_3 = ConvBlockEnc(kernel_size=(3, 3, 3), filters=32)
         self.convenc_4 = ConvBlockEnc(kernel_size=(3, 3, 3), filters=64)
         self.convenc_5 = ConvBlockEnc(kernel_size=(3, 3, 3), filters=128)
         self.convenc_6 = ConvBlockEnc(kernel_size=(3, 3, 3), filters=256)
 
-        self.maxpool = MaxPool3D(pool_size=(2, 2, 2))
+        self.maxpool = MaxPooling3D(pool_size=(2, 2, 2))
 
         self.dconv_1 = ConvBlockDec(kernel_size=(3, 3, 3), filters=128)
         self.dconv_2 = ConvBlockDec(kernel_size=(3, 3, 3), filters=64)
@@ -27,4 +30,79 @@ class DualCNNLstm(tf.keras.models.Model):
 
         self.upsample = UpSampling3D(size=(2, 2, 2))
 
-        self.lstm = Lstm(kernel_size=(2, 2), filters=256)
+        self.lstm_encode = Lstm(kernel_size=(3, 3), filters=4)
+        self.lstm_decode = Lstm(kernel_size=(3, 3), filters=4)
+        self.lstm_down = Lstm(kernel_size=(3, 3), filters=256)
+
+        self.add = Add()
+        self.concatenate = Concatenate()
+
+        self.output_layer = Final(kernel_size=(3, 3, 3), filters=4)
+
+    def call(self, input, training=False, **kwargs):
+        # Forward pass
+
+        # encoding step
+        # initial embedding
+        x11 = self.convenc_start(input)
+
+        # pass it to lstm
+        x11 = self.lstm_encode(x11)
+        x11_down = self.maxpool(x11)
+
+        x21 = self.convenc_1(x11_down)
+        x21_down = self.maxpool(x21)
+
+        x31 = self.convenc_2(x21_down)
+        x31_down = self.maxpool(x31)
+
+        x41 = self.convenc_3(x31_down)
+        x41_down = self.maxpool(x41)
+
+        x51 = self.convenc_4(x41_down)
+        x51_down = self.maxpool(x51)
+
+        x61 = self.convenc_5(x51_down)
+        x61_down = self.maxpool(x61)
+
+        x71 = self.convenc_6(x61_down)
+
+        # embeded lstm
+        xlstm = self.lstm_down(x71)
+
+        # decoding step
+        xlstm_up = self.upsample(xlstm)
+
+        x62 = self.dconv_1(xlstm_up)
+        x62_up = self.upsample(x62)
+
+        x52 = self.dconv_2(x62_up)
+        x52_up = self.upsample(x52)
+
+        x42 = self.dconv_3(x52_up)
+        x42_up = self.upsample(x42)
+
+        x32 = self.dconv_4(x42_up)
+        x32_up = self.upsample(x32)
+
+        x22 = self.dconv_5(x32_up)
+        x22_up = self.upsample(x22)
+
+        x12 = self.dconv_6(x22_up)
+
+        # decoding lstm
+        lstm_decode = self.lstm_decode(x12)
+
+        # classifier
+        classifier = self.output_layer(lstm_decode)
+
+        return classifier
+
+    def summary(self):
+        x = Input(shape=(128, 128, 128, 3))
+        model = Model(inputs=x, outputs=self.call(x))
+        return model.summary()
+
+
+model = DualPathCNNLstm()
+k = model.summary()
