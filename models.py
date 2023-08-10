59279919a593212b3cf12d6d8ad27b60739651c5
Deceptrax123 <sriniishsri@gmail.com>
2023-08-10 23:40:54 +0530

    chnages to model

--- models.py
+++ models.py
@@ -20,7 +20,12 @@ class DualPathCNNLstm(tf.keras.Model):
         self.convenc_5 = ConvBlockEnc(kernel_size=(3, 3, 3), filters=128)
         self.convenc_6 = ConvBlockEnc(kernel_size=(3, 3, 3), filters=256)
 
-        self.maxpool = MaxPooling3D(pool_size=(2, 2, 2))
+        self.maxpool1 = MaxPooling3D(pool_size=(2, 2, 2))
+        self.maxpool2 = MaxPooling3D(pool_size=(2, 2, 2))
+        self.maxpool3 = MaxPooling3D(pool_size=(2, 2, 2))
+        self.maxpool4 = MaxPooling3D(pool_size=(2, 2, 2))
+        self.maxpool5 = MaxPooling3D(pool_size=(2, 2, 2))
+        self.maxpool6 = MaxPooling3D(pool_size=(2, 2, 2))
 
         self.dconv_0 = ConvBlockDec(kernel_size=(3, 3, 3), filters=256)
         self.dconv_1 = ConvBlockDec(kernel_size=(3, 3, 3), filters=128)
@@ -30,13 +35,23 @@ class DualPathCNNLstm(tf.keras.Model):
         self.dconv_5 = ConvBlockDec(kernel_size=(3, 3, 3), filters=8)
         self.dconv_6 = ConvBlockDec(kernel_size=(3, 3, 3), filters=4)
 
-        self.upsample = UpSampling3D(size=(2, 2, 2))
+        self.upsample1 = UpSampling3D(size=(2, 2, 2))
+        self.upsample2 = UpSampling3D(size=(2, 2, 2))
+        self.upsample3 = UpSampling3D(size=(2, 2, 2))
+        self.upsample4 = UpSampling3D(size=(2, 2, 2))
+        self.upsample5 = UpSampling3D(size=(2, 2, 2))
+        self.upsample6 = UpSampling3D(size=(2, 2, 2))
 
         self.lstm_encode = Lstm(kernel_size=(3, 3), filters=4)
         self.lstm_decode = Lstm(kernel_size=(3, 3), filters=4)
         self.lstm_down = Lstm(kernel_size=(3, 3), filters=256)
 
-        self.add = Add()
+        self.add1 = Add()
+        self.add2 = Add()
+        self.add3 = Add()
+        self.add4 = Add()
+        self.add5 = Add()
+        self.add6 = Add()
 
         self.output_layer = Final(kernel_size=(3, 3, 3), filters=4)
 
@@ -49,22 +64,22 @@ class DualPathCNNLstm(tf.keras.Model):
 
         # pass it to lstm
         x11 = self.lstm_encode(x11)
-        x11_down = self.maxpool(x11)
+        x11_down = self.maxpool1(x11)
 
         x21 = self.convenc_1(x11_down)
-        x21_down = self.maxpool(x21)
+        x21_down = self.maxpool2(x21)
 
         x31 = self.convenc_2(x21_down)
-        x31_down = self.maxpool(x31)
+        x31_down = self.maxpool3(x31)
 
         x41 = self.convenc_3(x31_down)
-        x41_down = self.maxpool(x41)
+        x41_down = self.maxpool4(x41)
 
         x51 = self.convenc_4(x41_down)
-        x51_down = self.maxpool(x51)
+        x51_down = self.maxpool5(x51)
 
         x61 = self.convenc_5(x51_down)
-        x61_down = self.maxpool(x61)
+        x61_down = self.maxpool6(x61)
 
         x71 = self.convenc_6(x61_down)
 
@@ -73,30 +88,30 @@ class DualPathCNNLstm(tf.keras.Model):
 
         # decoding step and bring in the skip connections
         xlstm = self.dconv_0(xlstm)
-        xlstm_up = self.upsample(xlstm)
+        xlstm_up = self.upsample1(xlstm)
 
         x62 = self.dconv_1(xlstm_up)
-        x62 = self.add([x62, x61])
-        x62_up = self.upsample(x62)
+        x62 = self.add1([x62, x61])
+        x62_up = self.upsample2(x62)
 
         x52 = self.dconv_2(x62_up)
-        x52 = self.add([x51, x52])
-        x52_up = self.upsample(x52)
+        x52 = self.add2([x51, x52])
+        x52_up = self.upsample3(x52)
 
         x42 = self.dconv_3(x52_up)
-        x42 = self.add([x41, x42])
-        x42_up = self.upsample(x42)
+        x42 = self.add3([x41, x42])
+        x42_up = self.upsample4(x42)
 
         x32 = self.dconv_4(x42_up)
-        x32 = self.add([x31, x32])
-        x32_up = self.upsample(x32)
+        x32 = self.add4([x31, x32])
+        x32_up = self.upsample5(x32)
 
         x22 = self.dconv_5(x32_up)
-        x22 = self.add([x22, x21])
-        x22_up = self.upsample(x22)
+        x22 = self.add5([x22, x21])
+        x22_up = self.upsample6(x22)
 
         x12 = self.dconv_6(x22_up)
-        x12 = self.add([x12, x11])
+        x12 = self.add6([x12, x11])
 
         # decoding lstm
         lstm_decode = self.lstm_decode(x12)
